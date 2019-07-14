8d9c2c575f6637841eca0a887d71bf9a57afcacc
Sannikov Sergei <serega.sann@mail.ru>
2019-07-15 01:32:43 +0500

    Изменения работы с config файлом

---
+++ TestTask/ConfigSettings.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TestTask
+{
+    class ConfigSettings
+    {
+        public static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        public static void ChangeSettings()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите параметр key, который хотите изменить или введите новый, если хотите его создать");
+                string key = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите параметр value");
+                string value = Convert.ToString(Console.ReadLine());
+                AddUpdateAppSettings(key, value);
+
+                Console.WriteLine("Хотите изменить еще один параметр?\nВведите: 1 - да, 2 - нет");
+                string param = Convert.ToString(Console.ReadLine());
+                if (param == "2")
+                {
+                    Console.WriteLine("Параметры изменены\n");
+                    break;
+                }
+                else if (param == "1") Console.WriteLine("Параметры изменены\nСледующий параметр: ");
+                else Console.WriteLine("Введено неверное значение");
+            }
+        }
+    }
+}
--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -12,18 +12,36 @@ namespace TestTask
         {
             try
             {
-
                 //ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
                 //configMap.ExeConfigFilename = @"App1.config";
                 //Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
 
                 if (args.Length == 0)
-                {
+                {                    
                     Console.WriteLine("Приложение было запущено без параметра");
-                    new Summon().call(new SiteCeator());    //Sites.CheckAvailability();
-                    new Summon().call(new MSQLCreator());   //MsSQL.CheckAvailability();
 
-                    SendigToEmail.SendMessage("SiteFile.json", "FileSQLServer.json");
+                    Console.WriteLine("Параметры файла конфигурации");
+                    ConfigSettings.ReadAllSettings();
+                    Console.WriteLine();
+
+                    Console.WriteLine("Вы хотите изменить параметры запуска?\nВведите: 1-да, 2-нет");
+                    string param = Convert.ToString(Console.ReadLine());
+                    if (param == "1")
+                    {
+                        ConfigSettings.ChangeSettings();
+                        new Summon().call(new SiteCeator());    //Sites.CheckAvailability();
+                        new Summon().call(new MSQLCreator());   //MsSQL.CheckAvailability();
+
+                        SendigToEmail.SendMessage("SiteFile.json", "FileSQLServer.json");
+                    }
+                    else if (param == "2")
+                    {
+                        new Summon().call(new SiteCeator());    //Sites.CheckAvailability();
+                        new Summon().call(new MSQLCreator());   //MsSQL.CheckAvailability();
+
+                        SendigToEmail.SendMessage("SiteFile.json", "FileSQLServer.json");
+                    }
+                    else Console.WriteLine("Должно быть введено 1 или 2");
                 }
                 else
                 {
